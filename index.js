e855539ec73472433a197322cce8cc589294f17e
Juan José Gamboa Gutiérrez <66813179+JuanJGGtz@users.noreply.github.com>
2023-05-31 22:12:34 -0600

    PARA APLICAR LOS CAMBIOS DE SERVIR INDEX

--- index.js
+++ index.js
@@ -2,41 +2,29 @@ const express = require('express');
 const { dbConnection } = require('./database/config');
 const cors = require('cors')
 require('dotenv').config();
-
 //Ver todos los procesos que extá corriendo node
 //console.log('process.env', process.env)
-
 //Crear el servidor de express
 const app = express();
-
-
 //BAse de datos
 dbConnection();
-
-
 //CORS
 app.use(cors());
-
 //Directorio Público
 //Middelware
 //Funcion que se ejecuta en el momento que alguien hace petición a mi servidor
 app.use(express.static('public'))
-
-
 //Lectura y parseo de body
 app.use(express.json());
-
 //Rutas
 app.use('/api/auth', require('./routes/auth'));
-
 //Eventos
 app.use('/api/events', require('./routes/events'));
-
 //AGREGAR PARA SOLUCIONAR PROBLEMA DE DESPLIEGUE EN EL SERVIDOR "Cannot GET /auth/login"
+//HAZ LOS CAMBIOS
 app.get('*', (req, res) => {
     res.sendFile(__dirname + '/public/index.html');
 });
-
 //Escuchar peticiones
 app.listen(process.env.PORT, () => {
     console.log('Servidor corriendo en el puerto ' + process.env.PORT);
